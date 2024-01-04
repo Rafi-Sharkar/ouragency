91f05eb1f292e74f321473d799bccd28e2640aba
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2024-01-04 11:50:05 +0600

    nav done

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Nav from './web/GlobalComponents/Nav'
 import Home from "./web/Pages/Home/Home"
+import About from "./web/Pages/About/About"
+import Work from "./web/Pages/Work/Work"
+import Services from "./web/Pages/Services/Services"
+import Contact from "./web/Pages/Contact/Contact"
 export default function Layout() {
   return (
     <>
-     <Home/>
+     <Nav/>
+     <Routes>
+       <Route path='/' element={<Home/>}/>
+       <Route path='/work' element={<Work/>}/>
+       <Route path='/services' element={<Services/>}/>
+       <Route path='/about' element={<About/>}/>
+       <Route path='/Contact' element={<Contact/>}/>
+     </Routes>
+     
     </>
   )
 }
--- src/web/GlobalComponents/Nav.jsx
+++ src/web/GlobalComponents/Nav.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from "./Nav.module.css"
+
+import { FaHome } from "react-icons/fa";
+import { SiAboutdotme } from "react-icons/si";
+import { MdContactMail,MdMiscellaneousServices,MdDesignServices } from "react-icons/md";
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <>
+    <div className='fixed top-[35%] right-[0%] min-h-[100vh] bg-transparent'>
+      <div className='flex flex-col justify-center items-center gap-[5px] relative border-solid border-[3px]'>
+         <div className={`flex justify-center items-center relative ${styles.haha}`}>
+          <Link to="/" className={styles.linkicons}><FaHome /></Link>
+          <div className={styles.linktitle}>Home</div>
+         </div>
+         <div className={`flex justify-center items-center ${styles.haha}`}>
+          <Link to="/work" className={styles.linkicons}><MdDesignServices/></Link>
+          <div className={styles.linktitle}>Work</div>
+         </div>
+         <div className={`flex justify-center items-center ${styles.haha}`}>
+          <Link to="/services" className={styles.linkicons}><MdMiscellaneousServices/></Link>
+          <div className={styles.linktitle}>Services</div>
+         </div>
+         <div className={`flex justify-center items-center ${styles.haha}`}>
+          <Link to="/about" className={styles.linkicons}><SiAboutdotme /></Link>
+          <div className={styles.linktitle}>About</div>
+         </div>
+         <div className={`flex justify-center items-center ${styles.haha}`}>
+          <Link to="/contact" className={styles.linkicons}><MdContactMail /></Link>
+          <div className={styles.linktitle}>Contact</div>
+         </div>
+
+      </div>
+    </div>
+    </>
   )
 }
