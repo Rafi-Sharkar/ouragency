66ece336c1806045fdec3b4b2cea1648f347f28f
Rafi Sharkar <rafisharkar144@gmail.com>
2024-01-11 12:04:20 +0600

    one third work complited of service page

--- src/web/Pages/Services/Services.jsx
+++ src/web/Pages/Services/Services.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import style from './Services.module.css'
+import {icons} from './data'
+import img1 from './images/bg_01.jpg'
 
 export default function Services() {
   return (
-    <div>
-      <h1>Services</h1>
-      <h2>Get start</h2>
-    </div>
+    <>
+  <div className="border-[1rem] bg-[img1]">
+    <h3>Perfection of work</h3>
+    <h1>Our purpose is improved your business experience</h1>
+    <button>Start Now</button>
+  </div>
+  
+  <div className={`${style.infinite} ${style.highway_slider}`}>
+		<div className={`${style.container} ${style.highway_barrier}`}>
+			<ul className={style.highway_lane}>
+				{icons.map((icon,i)=> {<li className={style.highway_car}><img className={style.image} src={icon.img} key={i}/></li>})}
+							
+			</ul>
+		</div>
+	</div>
+
+  </>
+    
   )
 }
+
